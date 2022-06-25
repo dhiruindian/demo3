15f0f91cdf888aec9dc8d377dbb314de26d563e5
dhiruindian <dhiruindian@gmail.com>
2022-06-25 13:05:39 +0530

    added new line

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -8,6 +8,7 @@ public class Demo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo3Application.class, args);
+		System.out.println("Welcome to Java");
 	}
 
 }
